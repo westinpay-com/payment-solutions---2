01007805e88d0f40938ee7abcbb3b30965968f4e
westinpay transfer | westinpay.com <153361307+westinpay-com@users.noreply.github.com>
2024-02-13 21:52:31 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// A unique order number is created for each customer
+function generateOrderNumber(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+document.getElementById('order_number').value = generateOrderNumber(10);
